97e33a0efc793ea38bcd8083514e96b8a9506085
AlfredOfficial <orapaalfred7@gmail.com>
2025-05-19 21:34:57 +0800

    update this folder, add extends block, and logout

--- finalProject/staffApp/urls.py
+++ finalProject/staffApp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from . import views
 
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('dashboard/', views.dashboard, name = 'dashboard'),
     path('ajax/get-roles/', views.get_roles, name='get_roles'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
